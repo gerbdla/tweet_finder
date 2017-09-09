023395df5c1385b670ae75cdbbd56a7a1b74f89e
DAVID GERBER <gerbdla@gmail.com>
2017-09-09 12:58:11 -0700

    remove debug statements

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -5,9 +5,8 @@ class Api::V1::TweetsController < Api::V1::BaseController
 
   def create
     service_tweets = Tweet.get_tweets(params)
-   puts "This is it"
-    puts service_tweets.inspect
-    if service_tweets.key?("error") || service_tweets.key?("errors") || service_tweets.parsed_response.empty?
+
+    if (service_tweets.key?("error") || service_tweets.key?("errors") || service_tweets.parsed_response.empty?)
       tweets = {tweet_text: "No Tweets Were Found For #{params[:user][:name]}", id: 1}
     else
       user = User.where(username: params[:user][:name]).first
